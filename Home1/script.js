320a0117ae417d85046c846fdfcac9b171b5761c
Ivan M <slade91@mail.ru>
2024-01-13 20:57:18 +0300

    change home1

--- Home1/script.js
+++ Home1/script.js
@@ -3,37 +3,40 @@ let counterMain = 0;
 let counterAbout = 0;
 let counter404 = 0;
 
-const msgModule = msg =>{
-    if([11, 12, 13, 14].includes(msg % 100) || msg % 10 === 1){
+const msgModule = msg => {
+    if ([11, 12, 13, 14].includes(msg % 100) || msg % 10 === 1) {
         return `Вы вы были здесь ${msg} раз`;
-    }else if([2,3,4].includes(msg % 10)){
+    } else if ([2, 3, 4].includes(msg % 10)) {
         return `Вы вы были здесь ${msg} раза`;
-    }else {
+    } else {
         return `Вы вы были здесь ${msg} раз`;
-    }    
+    }
 }
 
 const server = http.createServer((req, res) => {
     console.log('Запрос получен');
     if (req.url === '/') {
         counterMain++;
-        res.writeHead(200,{
+        res.writeHead(200, {
             'Content-type': 'text/html; charset=UTF-8',
         });
-        res.end(`<h1>Добро пожаловать на мой сайт!! ${msgModule(counterMain)}</h1>`);
-    }else if(req.url === '/about'){
+        res.end(`<h1>Добро пожаловать на мой сайт!!</h1><br><h3> ${msgModule(counterMain)} </h3><br>
+            <a href="http://localhost:3000/about">Go to about</a>`);
+    } else if (req.url === '/about') {
         counterAbout++;
-        res.writeHead(200,{
+        res.writeHead(200, {
             'Content-type': 'text/html; charset=UTF-8',
         });
-        res.end(`<h1>Обо мне:) ${msgModule(counterAbout)}</h1>`);
-    }else{
+        res.end(`<h1>Обо мне:) </h1><br><h3> ${msgModule(counterAbout)} </h3><br>
+            <a href="http://localhost:3000">Go to Main</a>`);
+    } else {
         counter404++;
-        res.writeHead(404,{
+        res.writeHead(404, {
             'Content-type': 'text/html; charset=UTF-8',
         });
-        res.end(`<h1>Страница не найдена!!!404 ${msgModule(counter404)}</h1>`);
-    }   
+        res.end(`<h1>Страница не найдена!!!ошибка 404</h1><br><h3> ${msgModule(counter404)} <h3><br>
+            <a href="http://localhost:3000">Go to Main</a>`);
+    }
 });
 
 const port = 3000;
