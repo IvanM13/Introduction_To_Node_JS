95128bf56d0e8dba844dc3a0ce82ad8c625d58f1
Ivan M <slade91@mail.ru>
2024-01-17 17:56:41 +0300

    change Seminar2

--- sem2/script.js
+++ sem2/script.js
@@ -1,10 +1,10 @@
-function calculateResultSum(purchases, discount) {
-    let total = purchases.reduce((acc, purchases) => acc += purchases, 0);
+// import calculateResultSum from './calc.js';
+const calculateResultSum = require('./calc.js');
+require('colors');
 
-    total = total * discount;
-    return total;
-}
 
-const total = calculateResultSum([12.1, 32.2, 43.1], 0.9);
+const total = calculateResultSum([12.1, 52.2, 73.1], 0.9);
 
-console.log('Общая стоимость покупок: ' + total + ' рублей');
+const text = 'Общая стоимость покупок: ' + total.toString().blue + ' рублей'.yellow;
+
+console.log((total > 50) ? text.red : text.green);
